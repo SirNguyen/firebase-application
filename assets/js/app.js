d3bcab8cfeaf4da08aa69f1c9d63a5d3cdc4b596
nVar <nkeledjian@gmail.com>
2018-10-27 12:15:41 -0700

    Updated app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,5 @@
 // Initialize Firebase
 
-var database = firebase.database();
-
 var config = {
     apiKey: "AIzaSyCCXDBvWPE-ldjI9NqCVoe4Vom-IjM_jAM",
     authDomain: "in-class-project-e3bb6.firebaseapp.com",
@@ -12,13 +10,32 @@ var config = {
   };
   firebase.initializeApp(config);
 
+var dataRef = firebase.database();  
+
+var name = "";
+var Prate = "";
+var startDate = "";
+var monthRate = "";
+
 
   // point to HTML handling input
-$("#employee-input").on("click", function() {
-    // initial row div
-    dataDiv = $("<div>");
-    dataDiv.attr("class", "row");
-    var input = $("#employee-input").val().trim();
-    // push function
-    database.push(input);
-})
+$("#add-employee").on("click", function(event) {
+
+    event.preventDefault();
+
+    name = $("#input-name").val();
+    rate = $("#input-rate").val();
+    date = $("#input-date").val();
+    monthRate = $("#input-monthly").val();
+
+    dataRef.ref().push( {
+            name: name,
+            Prate: Prate,
+        });
+
+
+});
+
+
+    
+    
