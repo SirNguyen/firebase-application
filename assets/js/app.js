c68b8f8c84d43c9000cea65e9892f22bfb2f6ddd
nVar <nkeledjian@gmail.com>
2018-10-27 10:44:25 -0700

    Setup firebase configuration

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,14 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCCXDBvWPE-ldjI9NqCVoe4Vom-IjM_jAM",
+    authDomain: "in-class-project-e3bb6.firebaseapp.com",
+    databaseURL: "https://in-class-project-e3bb6.firebaseio.com",
+    projectId: "in-class-project-e3bb6",
+    storageBucket: "",
+    messagingSenderId: "65547330676"
+  };
+  firebase.initializeApp(config);
+
 (document).on("click", function() {
     // initial row div
     dataDiv = $("<div>");
