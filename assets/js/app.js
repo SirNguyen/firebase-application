e7a7e0c72ecc154b2ebe276a7a0afd8ee2033c70
nVar <nkeledjian@gmail.com>
2018-10-27 11:21:57 -0700

    Updates to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,18 +7,18 @@ var config = {
     authDomain: "in-class-project-e3bb6.firebaseapp.com",
     databaseURL: "https://in-class-project-e3bb6.firebaseio.com",
     projectId: "in-class-project-e3bb6",
-    storageBucket: "",
+    storageBucket: "in-class-project-e3bb6.appspot.com",
     messagingSenderId: "65547330676"
   };
   firebase.initializeApp(config);
 
 
   // point to HTML handling input
-$("input").on("click", function(snapshot) {
+$("#employee-input").on("click", function() {
     // initial row div
     dataDiv = $("<div>");
     dataDiv.attr("class", "row");
-
+    var input = $("#employee-input").val().trim();
     // push function
-    database.push()
+    database.push(input);
 })
